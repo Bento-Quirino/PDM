4ee92479ca926844bcd748c7665c9967d1c85405
Krhuhun <72083150+jorgelisboa@users.noreply.github.com>
2024-02-08 13:45:44 -0300

    Update exercicio-em-aula.js

--- Revisão Javascript/exercicio-em-aula.js
+++ Revisão Javascript/exercicio-em-aula.js
@@ -1,24 +1,45 @@
-// Criar variável
-let nome = "Jorge"
-const dataDeNscimento = "27/02/2004"
+let pessoa = {
+    // String
+    //Number
+    // Boolean (true/false)
+    // array de objetos
+    materiasCursadas: [
+        {
+            nome: "TIA",
+            horasCursadas: 100
+        }
+    ]
+}
+
 
 const chamada = [
-"João",
-  "Jorge",
+    {
+        nome: "Matheus"
+    },
+    {
+        nome: "Richard"
+    },
 ]
 
-console.log(chamada)
-const chamadaComObjetos = [
-  {
-    nome: "Jorge",
-    idade: 19,
-    ra: "220077",
-  },
-]
+console.log(pessoa.materiasCursadas[0].horasCursadas)
 
-console.log(chamadaComObjetos) //Mostra td
-console.log(chamadaComObjetos[0].nome) // Mostra só o nome
+// Mostra todos os nome da chamada com o for
+for (let i = 0; i < chamada.length; i++) {
+    console.log(i + ') ' + chamada[i].nome)
+}
 
+// Mostra todos os nomes da chamada com o forEach
+chamada.forEach(aluno => { 
+    console.log(aluno.nome)
+})
+
+// Mostra todos os nomes da chamada com o map
+chamada.map(aluno => { 
+    console.log(aluno.nome)
+})
+
+
+
+// EXERCÍCIOS
 // Mostre todos os nomes em ordem alfabética)
-
 // Mostre todas as idade acima de 18 (if-else e for)
