e5eefe73888a0eb0bc585b4ae6ba19164ba4c84f
Krhuhun <72083150+jorgelisboa@users.noreply.github.com>
2024-02-08 11:58:33 -0300

    Create exercicio-em-aula.js

---
+++ Revisão Javascript/exercicio-em-aula.js
@@ -0,0 +1,24 @@
+// Criar variável
+let nome = "Jorge"
+const dataDeNscimento = "27/02/2004"
+
+const chamada = [
+"João",
+  "Jorge",
+]
+
+console.log(chamada)
+const chamadaComObjetos = [
+  {
+    nome: "Jorge",
+    idade: 19,
+    ra: "220077",
+  },
+]
+
+console.log(chamadaComObjetos) //Mostra td
+console.log(chamadaComObjetos[0].nome) // Mostra só o nome
+
+// Mostre todos os nomes em ordem alfabética)
+
+// Mostre todas as idade acima de 18 (if-else e for)
